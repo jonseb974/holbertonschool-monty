cd262aa202d93427f2b436fe481864e80a9f85eb
william0863 <granger.w35@gmail.com>
2022-08-17 10:18:59 +0200

    adding files

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * main - main function
+ *
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+
+{
+
+}
--- monty.h
+++ monty.h
@@ -1,8 +1,8 @@
 #ifndef MONTY_H
 #define MONTY_H
 
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -13,7 +13,9 @@
  * Description: doubly linked list node structure
  * for stack, queues, LIFO, FIFO
  */
+
 typedef struct stack_s
+
 {
 	int n;
 	struct stack_s *prev;
@@ -28,7 +30,9 @@ typedef struct stack_s
  * Description: opcode and its function
  * for stack, queues, LIFO, FIFO
  */
+
 typedef struct instruction_s
+
 {
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
